d467240391aea82d9da78e359ad74cc7b86baf4e
SuperCoder <propscode254@gmail.com>
2023-10-15 12:06:19 +0200

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // import { api_key } from "/config.js";
 import { countries } from "/assets/db/countries.js";
 
-const myKey = env.API_KEY;
+const myKey = process.env.API_KEY;
 
 const cityInput = document.querySelector('[data-js="city"]');
 const searchButton = document.querySelector('[data-js="search"]');
